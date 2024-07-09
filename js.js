8d403532d2dc4443958ff2a901e85f0d5206d18b
huyluong26 <160562478+huyluong26@users.noreply.github.com>
2024-07-09 17:00:46 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,169 @@
+$(document).ready(function(){
+    $("#anh1").mouseover(function(){
+            $(this).show();
+            $(this).html('<img src="sach1-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh1").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach1.jpg" alt="" width="250px" height="350px">');
+        });
+  });
+  
+  $(document).ready(function(){
+    $("#anh2").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach2-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh2").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach2.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  $(document).ready(function(){
+    $("#anh3").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach3-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh3").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach3.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  $(document).ready(function(){
+    $("#anh4").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach4-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh4").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach4.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  $(document).ready(function(){
+    $("#anh5").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach5-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh5").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach5.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  $(document).ready(function(){
+    $("#anh6").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach6-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh6").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach6.jpg" alt="" width="250px" height="350px">');
+        });
+  });
+  $(document).ready(function(){
+    $("#anh7").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach7-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh7").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach7.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  ///////
+  
+  $(document).ready(function(){
+    $("#anh8").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach8-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh8").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach8.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  
+  $(document).ready(function(){
+    $("#anh9").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach9-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh9").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach9.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  $(document).ready(function(){
+    $("#anh10").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach10-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh10").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach10.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  $(document).ready(function(){
+    $("#anh11").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach11-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh11").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach11.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  $(document).ready(function(){
+    $("#anh12").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach12-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh12").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach12.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  $(document).ready(function(){
+    $("#anh13").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach13-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh13").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach13.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  $(document).ready(function(){
+    $("#anh14").hover(function(){
+            $(this).show();
+            $(this).html('<img src="sach14-2.jpg"  alt="" width="250px" height="350px">');
+        })
+    
+    $("#anh14").mouseout(function(){
+            $(this).show();
+            $(this).html('<img src="sach14.jpg"  alt="" width="250px" height="350px">');
+        });
+  });
+  //////////
+  $(document).ready(function(){
+              $(".tensp").on({
+                  mouseenter: function(){
+                  $(this).css({"color":"blue"});
+                  },
+                  mouseleave: function(){
+                  $(this).css({"color":"black"});
+                  },
+              });
+          });
